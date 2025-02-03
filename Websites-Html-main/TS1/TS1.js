1cdda2c10e2486f91824a803c607f663e987ff7e
Yanela-1 <151740862+Yanela-1@users.noreply.github.com>
2025-02-03 15:56:26 +0800

    Add files via upload

---
+++ Websites-Html-main/TS1/TS1.js
@@ -0,0 +1,9 @@
+function changeColor(){
+    let container = document.getElementById("container");
+    if(container.style.backgroundColor == "white"){
+        container.style.backgroundColor = "red";
+    }
+    else{
+        container.style.backgroundColor = "white";
+    }
+}
